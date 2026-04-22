daa9199ff4afd27dccebd8455679d0d014fa1edb
yeonsm1 <ysmysm54321@gmail.com>
2026-04-22 20:49:56 +0900

    Test2

--- Assets/Second.cs
+++ Assets/Second.cs
@@ -7,7 +7,7 @@ public class Second : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Number2 Hello World");
     }
 
     // Update is called once per frame
